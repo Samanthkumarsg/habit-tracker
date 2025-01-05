b7a318b4df73ffe56246ae67fc2ce523a3d6a17e
samanth kumar <samanthkumarsg@gmail.com>
2025-01-05 02:32:51 +0100

    Fix/Initial UI component spacing

--- layouts/LayoutWrapper.js
+++ layouts/LayoutWrapper.js
@@ -1,6 +1,6 @@
 const LayoutWrapper = ({ children }) => {
     return (
-        <div className="p-6 md:p-0 lg:p-0 xl:p-0">
+        <div className="p-6 xs:p-6 sm:p-6 md:p-0 lg:p-0 xl:p-0">
             {children}
         </div>
     )
