408fe14e3b66efa8c4ea3107cad25245c0e61d25
samanth kumar <samanthkumarsg@gmail.com>
2025-01-05 02:31:08 +0100

    Fix/Initial UI component spacing

--- app/components/Categories/CategoryCard.js
+++ app/components/Categories/CategoryCard.js
@@ -3,7 +3,7 @@ import { FiTrash2, FiGrid } from "react-icons/fi";
 const CategoryCard = (props) => {
     const { name } = props;
     return (
-        <div className="border w-full sm:max-w-96 md:max-w-64 p-8 flex flex-row justify-between rounded-2xl shadow-sm hover:shadow-md cursor-pointer ">
+        <div className="border bg-white w-full sm:max-w-96 md:max-w-64 p-8 flex flex-row justify-between rounded-2xl shadow-sm hover:shadow-md cursor-pointer ">
             <div className="flex flex-col justify-between">
                 <FiGrid className="text-3xl text-gray-500 font-normal stroke" />
                 <h1 className="text-xl text-gray-900 font-semibold mt-4">{name}</h1>
